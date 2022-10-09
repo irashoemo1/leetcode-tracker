1b871316cf2ce2238135f73d6bce0e3572a1d4bb
Ira Shoemo <irashoemo1@gmail.com>
2022-10-08 20:15:58 -0400

    changes

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_STRING, {
+    const conn = await mongoose.connect("mongodb+srv://irashoemo1:leetheecoder@cluster0.fpekx.mongodb.net/test", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     //   useFindAndModify: false,
